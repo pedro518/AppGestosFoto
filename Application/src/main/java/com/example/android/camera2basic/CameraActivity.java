6d7a84304409105e84dc8082648b71b867fda0df
Pedro518 <pedro_518@hotmail.com>
2016-02-07 11:21:47 +0100

    Añadida documentación

--- Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -1,17 +1,16 @@
-/*
- * Copyright 2014 The Android Open Source Project
+/**
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.example.android.camera2basic;
@@ -30,6 +29,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+/**
+ * <p>Clase que se encarga de asociar las acciones del usuario con la ejecución de las ordenes correspondientes</p>
+ *
+ * @author Pedro Antonio Ruiz Cuesta
+ * @author Ignacio Martín Requena
+ * Última modificación: 7/2/2016
+ */
 
 public class CameraActivity extends AppCompatActivity implements GestureOverlayView.OnGesturePerformedListener{
 
